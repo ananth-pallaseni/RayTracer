635eb1e8f29f5ff3b61214d36470d10e854a2998
ananth-pallaseni <ananth.pallaseni@gmail.com>
2014-10-26 21:33:34 -0700

    fixObjects WIP 33

--- Sampler.h
+++ Sampler.h
@@ -8,7 +8,7 @@ using namespace Eigen;
 class Sampler {
 public:
 	int numPixX, numPixY;
-	Vector2f UL, UR, LR, LL;
+	Vector3f UL, UR, LR, LL;
 	int currX, currY;
 	float unitX, unitY;
 	 
@@ -21,10 +21,10 @@ public:
 	Sampler(int pixX, int pixY, float ulx, float uly, float urx, float ury, float lrx, float lry, float llx, float lly) {
 		numPixX = pixX;
 		numPixY = pixY;
-		UL << ulx, uly;
-		UR << urx, ury;
-		LR << lrx, lry;
-		LL << llx, lly;
+		UL << ulx, uly, 0;
+		UR << urx, ury, 0;
+		LR << lrx, lry, 0;
+		LL << llx, lly, 0;
 		currX = -1;
 		currY = 0;
 		unitX = (urx - ulx) / pixX;
