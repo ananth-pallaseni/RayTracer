43b74d96b3659cdfa9466dc50d017d47f1fc7190
ananth-pallaseni <ananth.pallaseni@gmail.com>
2014-10-29 00:03:57 -0700

    shadows

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -124,7 +124,7 @@ color RayTracer::shade(Vector3f pointOnShape, Vector3f normalAtPoint, object sha
 			Vector3f ttt(50, 50, 50);
 			rgb = rgb + diffuse(normalAtPoint, lightDirection, shape.mat.diff, pl.l());
 			rgb = rgb + specular(normalAtPoint, lightDirection, e, shape.mat.spec, pl.l(), shape.mat.phongExp);
-			rgb = rgb + 2 * ambient(pl.l(), shape.mat.amb);
+			rgb = rgb + ambient(pl.l(), shape.mat.amb);
 		}
 	}
 
