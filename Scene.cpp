5efd12851c3461699eed2d73363fdc608f41b7a2
ananth-pallaseni <ananth.pallaseni@gmail.com>
2014-10-29 00:20:31 -0700

    shadows

--- Scene.cpp
+++ Scene.cpp
@@ -144,7 +144,7 @@ int main(int argc, char* argv[])
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	// Transform test:
-	Vector3f vv(1, 1, 1, 0);
+	Vector4f vv(1, 1, 1, 0);
 	translate trans(3, 4, 5);
 	cout << trans.matrix << endl << endl;
 
