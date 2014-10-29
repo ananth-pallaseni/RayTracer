0b3ad4fad959d22900aeb45dcc84cee11fba8e79
ananth-pallaseni <ananth.pallaseni@gmail.com>
2014-10-29 03:27:34 -0700

    shadows

--- RayTracerObjects.h
+++ RayTracerObjects.h
@@ -365,7 +365,7 @@ struct ray
 		if (discriminant >= 0) {
 			// Only use negative value of discriminant, as this will be closer to the plane
 			float t = (-B - sqrt(discriminant)) / (2 * A);
-			Vector4f pointInWorldSpace = inv * p(eObj, sMinusEObj, t);
+			Vector4f pointInWorldSpace =  p(eObj, sMinusEObj, t);
 			(*point)(0) = pointInWorldSpace(0); // transform back into world coordinates
 			(*point)(1) = pointInWorldSpace(1);
 			(*point)(2) = pointInWorldSpace(2);
