c62594ea5a67f55c38cbea347475741d127653db
ananth-pallaseni <ananth.pallaseni@gmail.com>
2014-10-30 00:51:09 -0700

    trans

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -121,8 +121,8 @@ color RayTracer::shade(Vector3f pointOnShape, Vector3f normalAtPoint, object sha
 			Vector3f ttt(50, 50, 50);
 			rgb = rgb + diffuse(normalAtPoint, lightDirection, shape.mat.diff, pl.l());
 			rgb = rgb + specular(normalAtPoint, lightDirection, e, shape.mat.spec, pl.l(), shape.mat.phongExp);
-			rgb = rgb + ambient(pl.l(), shape.mat.amb);
 		}
+		rgb = rgb + ambient(pl.l(), shape.mat.amb);
 	}
 
 	for(int i = 0; i < numDirectionalLights; i++) {
@@ -133,8 +133,8 @@ color RayTracer::shade(Vector3f pointOnShape, Vector3f normalAtPoint, object sha
 
 			rgb = rgb + diffuse(normalAtPoint, lightDirection, shape.mat.diff, dl.l());
 			rgb = rgb + specular(normalAtPoint, lightDirection, e, shape.mat.spec, dl.l(), shape.mat.phongExp);
-			rgb = rgb + ambient(dl.l(), shape.mat.amb);
 		}
+		rgb = rgb + ambient(dl.l(), shape.mat.amb);
 	}
 
 	for(int i = 0; i < numAmbientLights; i++) {
