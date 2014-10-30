a45f7cba7f3180201c5d3568864c6cfb9ad8971b
ananth-pallaseni <ananth.pallaseni@gmail.com>
2014-10-30 03:55:41 -0700

    trans

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -177,7 +177,7 @@ color RayTracer::traceRay(ray r, int depth) {
 
 	// Check all Spheres
 	for(int i = 0; i < numSpheres; i++) {
-		if(r.intersect(spheres[i], &temp)) {
+		if(r.intersect(spheres[i], &temp, true)) {
 			hit = true;
 			//point = closest(point, temp); OLD
 			///////////////////////new
