3efcfcb4a93b36a0119dda151844f8acf81d6364
ananth-pallaseni <ananth.pallaseni@gmail.com>
2014-10-31 04:19:34 -0700

    refl

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -133,7 +133,7 @@ color RayTracer::shade(Vector3f pointOnShape, Vector3f normalAtPoint, object sha
 			Vector3f lightDirection = unit(pl.point - pointOnShape);
 	
 			Vector3f ttt(50, 50, 50);
-			//rgb = rgb + diffuse(normalAtPoint, lightDirection, shape.mat.diff, pl.l());
+			rgb = rgb + diffuse(normalAtPoint, lightDirection, shape.mat.diff, pl.l());
 			//rgb = rgb + specular(normalAtPoint, lightDirection, incoming.sMinusE, shape.mat.spec, pl.l(), shape.mat.phongExp);
 		}
 		if(depth < DEPTH_MAX ) {
