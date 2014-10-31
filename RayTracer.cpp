6cb2c36aa916eab20a0b26997a6c1e38114414ba
ananth-pallaseni <ananth.pallaseni@gmail.com>
2014-10-31 04:18:09 -0700

    refl

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -113,7 +113,7 @@ Vector3f RayTracer::reflectionRay(Vector3f point, Vector3f normalAtPoint, ray in
 	ray refl(point, incoming.sMinusE - 2 * normalAtPoint * (incoming.sMinusE.dot(normalAtPoint)), true);
 	color cTemp = traceRay(refl, depth + 1, point);
 	Vector3f c(cTemp.r * k_refl(0), cTemp.g * k_refl(1), cTemp.b * k_refl(2) );
-	c = c / 255;
+	c = c / 255; // as color rgb values are (0->255)
 	if((cTemp.b > 0 || cTemp.r > 0 )) {
 		cout << "SHADE CALC: " << (float) cTemp.r << ", " << (float)cTemp.g << ", " << (float)cTemp.b << endl;
 		cout << "REFL COLOR: " << c(0) << ", " << c(1) << ", " << c(2) << endl;
@@ -139,7 +139,7 @@ color RayTracer::shade(Vector3f pointOnShape, Vector3f normalAtPoint, object sha
 		if(depth < DEPTH_MAX ) {
 			rgb = rgb + reflectionRay(pointOnShape, normalAtPoint, incoming, shape.mat.refl, depth);
 		}
-		//rgb = rgb + ambient(pl.l(), shape.mat.amb);
+		rgb = rgb + ambient(pl.l(), shape.mat.amb);
 	}
 
 	for(int i = 0; i < numDirectionalLights; i++) {
