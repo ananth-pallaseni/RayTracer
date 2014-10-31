e35edc83cee1f4f2c87ec203a67947419e731390
ananth-pallaseni <ananth.pallaseni@gmail.com>
2014-10-31 07:50:53 -0700

    refl

--- RayTracerObjects.h
+++ RayTracerObjects.h
@@ -295,9 +295,9 @@ struct triangle : object
 		a << a4(0), a4(1), a4(2);
 		a << b4(0), b4(1), b4(2);
 		a << c4(0), c4(1), c4(2);
-		cout << "A: " << endl << a;
-		cout << "B: " << endl << b;
-		cout << "C: " << endl << c;
+		cout << "A: " << endl << a << endl << endl;
+		cout << "B: " << endl << b << endl << endl;
+		cout << "C: " << endl << c << endl << endl;
 	}
 };
 
