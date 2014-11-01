60c6dc4119a0cbd628b6c44a52507c264cce7ae5
ananth-pallaseni <ananth.pallaseni@gmail.com>
2014-10-31 19:08:40 -0700

    t

--- Scene.cpp
+++ Scene.cpp
@@ -102,7 +102,7 @@ void parseObj(const char* filename, material mat, Matrix4f objToWorld) {
     		float p3;
     		f3 >> p3;
     		Vector3f vertex(p1, p2, p3);
-    		cout << vertex << endl >> endl;
+    		cout << vertex << endl << endl;
           	vertices.push_back(vertex);
     	}
     }
