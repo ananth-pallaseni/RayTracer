5cd47412169e8dabdcc523c8ad61c3e219052933
ananth-pallaseni <ananth.pallaseni@gmail.com>
2014-11-02 10:52:06 -0800

    t

--- Scene.cpp
+++ Scene.cpp
@@ -491,7 +491,7 @@ material mm;
      0, 0, 0, 1;
 
      Vector3f p(10, 10, 0);
-     sphere sph(0, 0, 0, 10, mm, I, I);
+     sphere sph(0, 50, 0, 10, mm, I, I);
      cout << sphereNormal1(p, sph);
 
   /*material mm;
