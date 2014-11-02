1b478965802b5d1766bb967b9c2fcac3cefa0785
ananth-pallaseni <ananth.pallaseni@gmail.com>
2014-11-02 04:12:15 -0800

    t

--- Scene.cpp
+++ Scene.cpp
@@ -352,7 +352,8 @@ int main(int argc, char* argv[])
   cout << endl;
 
   aabbNode q(sph);
-  sphere qs = (sphere)q.contains();
+  sphere* qs = &q.contains();
+  cout << "QS RADIUS: " << qs.radius << endl;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
